99884987711755b00d97dd70fd459606c9f77a53
Ryan Smith <rms1000watt@gmail.com>
2018-08-24 16:21:33 -0700

    updated with path module

--- iam.tf
+++ iam.tf
@@ -1,9 +1,9 @@
 resource "aws_iam_role" "0" {
   name               = "${local.project_name}"
-  assume_role_policy = "${file("role-codebuild.json")}"
+  assume_role_policy = "${file("${path.module}/role-codebuild.json")}"
 }
 
 resource "aws_iam_role_policy" "0" {
   role   = "${aws_iam_role.0.name}"
-  policy = "${file("policy-codebuild.json")}"
+  policy = "${file("${path.module}/policy-codebuild.json")}"
 }
